859dc95500bbe939220b2c51c92110e3f32c1305
Christopher Buono <dev@epicbit.com>
2018-03-06 22:06:33 -0800

    Update helpers.php
    Adding in siteURL() function which returns website domain with protocol.

--- users/helpers/helpers.php
+++ users/helpers/helpers.php
@@ -237,3 +237,9 @@ function redirect($location){
 function output_message($message) {
 return $message;
 }
+
+function siteURL() {
+  $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+  $domainName = $_SERVER['HTTP_HOST'].'/';
+  return $protocol.$domainName;
+}
